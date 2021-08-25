02ca04a9094dc262b6b4a6db494f8a0bf51fd8c4
Manasvi Tallapalli <manasvirao2012@gmail.com>
2021-08-25 15:26:01 +0530

    search auto complete with changes

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "jquery"
-import "jquery-ui/autocomplete"
+require("jquery-ui")
 
 Rails.start()
 Turbolinks.start()
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   root 'welcome#index'
   get 'my_portfolio', to: 'users#my_portfolio'
   get 'search_stock', to: 'stocks#search'
-  get ‘index_stock’, to: ‘stocks#index’, as: ‘stocks’
+  
   get 'my_friends', to: 'users#my_friends'
   get 'search_friend', to: 'users#search'
 
